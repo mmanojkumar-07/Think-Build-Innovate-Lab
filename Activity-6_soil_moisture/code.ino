40fad4d6b23fcff1e6852b0af79d5d3386476ab4
Manojkumar M <m.manojkumar28122007@gmail.com>
2026-08-21 00:09:15 +0530

    Update code.ino

--- Activity-6_soil_moisture/code.ino
+++ Activity-6_soil_moisture/code.ino
@@ -1,1 +1,31 @@
+const int sensorPin = A0;
 
+const int redLED = 13;
+const int greenLED = 12;
+
+void setup() {
+  pinMode(redLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  int moisture = analogRead(sensorPin);
+
+  Serial.print("Moisture Value: ");
+  Serial.println(moisture);
+
+  if (moisture > 500) {
+    // Dry soil
+    digitalWrite(redLED, HIGH);
+    digitalWrite(greenLED, LOW);
+  }
+  else {
+    // Wet soil
+    digitalWrite(redLED, LOW);
+    digitalWrite(greenLED, HIGH);
+  }
+
+  delay(500);
+}
