318db8d32b9952e39deaab6ab8d91741282955fc
Manojkumar M <m.manojkumar28122007@gmail.com>
2026-09-08 12:15:10 +0530

    Update code.ino

--- Activity-09_Motor_control/code.ino
+++ Activity-09_Motor_control/code.ino
@@ -1,1 +1,37 @@
+int in1 = 8;
+int in2 = 7;
+int enable = 9;
 
+void setup()
+{
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enable, OUTPUT);
+}
+
+void loop()
+{
+  // Motor Forward
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(enable, HIGH);
+
+  delay(3000);
+
+  // Motor Stop
+  digitalWrite(enable, LOW);
+
+  delay(2000);
+
+  // Motor Reverse
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  digitalWrite(enable, HIGH);
+
+  delay(3000);
+
+  // Motor Stop
+  digitalWrite(enable, LOW);
+
+  delay(2000);
+}
